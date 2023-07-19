2fa306c2bed91c8f96b14d2b656b536c674968f9
Mshnwq <hayanx1000@gmail.com>
2023-07-19 22:11:43 +0300

    calendar script

---
+++ outlook_calendar.py
@@ -0,0 +1,82 @@
+import os
+import requests
+import time
+from datetime import datetime
+from openpyxl import load_workbook
+
+# Load the ACCESS_TOKEN from the .env file
+from dotenv import load_dotenv
+
+load_dotenv()
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+
+# Function to make the API request
+def add_event_to_calendar(event_body):
+    headers = {
+        "Authorization": f"Bearer {ACCESS_TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    endpoint = "https://graph.microsoft.com/v1.0/me/events"
+
+    try:
+        response = requests.post(endpoint, headers=headers, json=event_body)
+
+        if response.status_code == 200 or response.status_code == 201:
+            print("Event added successfully.")
+        else:
+            print(f"Failed to add event. Status code: {response.status_code}, Error: {response.text}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+def convert_to_iso_format(date_str):
+        try:
+            # Parse the date string into a datetime object
+            dt_object = datetime.strptime(str(date_str), '%d/%m/%Y')
+
+            # Set the time to 5 AM
+            dt_object = dt_object.replace(hour=5, minute=0, second=0, microsecond=0)
+
+            # Format the datetime object into ISO 8601 format
+            iso_date_str = dt_object.isoformat()
+            return iso_date_str
+        except ValueError:
+            return None
+
+if __name__ == "__main__":
+    # Load the Excel file
+    wb = load_workbook("dates.xlsx")
+    sheet = wb.active
+    events_list = []
+
+    # Add the events to the calendar
+    for row in sheet.iter_rows(min_row=7, values_only=True):  # Assuming until row 6 contains headers
+        title, start_date, end_date = row[1], row[5], row[8]
+        print(f"title: {title}")
+        print(f"start: {start_date}")
+        print(f"end: {end_date}")
+        if title is not None and start_date is not None and end_date is not None:
+
+            start_date_iso = convert_to_iso_format(start_date)
+            end_date_iso = convert_to_iso_format(end_date)
+
+            if start_date_iso is not None and end_date_iso is not None:
+                event_body = {
+                    "subject": title,
+                    "start": {
+                        "dateTime": start_date_iso,
+                        "timeZone": "UTC"
+                    },
+                    "end": {
+                        "dateTime": end_date_iso,
+                        "timeZone": "UTC"
+                    }
+                }
+                events_list.append(event_body)
+            else:
+                print("failed iso")
+
+    print(events_list)
+    for event in events_list:
+        add_event_to_calendar(event)
+        time.sleep(0.2)
